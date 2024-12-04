60c133094bfe7c704b74de6b177fd8015bc4582a
Marco Ris <ris@contria.com>
2024-12-04 09:47:37 +0100

    Adjust day3 to work

--- 2024/day03/day3.py
+++ 2024/day03/day3.py
@@ -1,26 +1,24 @@
-#!/usr/bin/env python3
-
-import sys
 import re
+import sys
 
-with open(sys.argv[1], 'r') as f:
-    lines = f.readlines()
 
-strings = "".join(lines)
+with open(sys.argv[1], "r") as f:
+    data = f.read()
 
-def mul(x,y):
-    return x * y
+part1 = 0
+part2 = 0
+enabled = True
+for inst in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", data):
+    match inst:
+        case "do()":
+            enabled = True
+        case "don't()":
+            enabled = False
+        case _:
+            x, y = map(int, inst[4:-1].split(","))
+            part1 += x * y
+            if enabled:
+                part2 += x * y
 
-pattern = r"mul\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)|do\(\)|don\'t\(\)"
-pattern_do = r"do\(\)"
-pattern_dont = r"don\'t\(\)"
-matches = re.findall(pattern, strings)
-print(strings)
-
-total_sum = sum(mul(int(x), int(y)) for x, y in matches)
-
-part1 = total_sum
-print(f'Part 1: {part1}')
-
-part2 = ""
-print(f'Part 2: {part2}')
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
